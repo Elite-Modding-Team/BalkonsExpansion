e2635b8c1af9618f6218c9e6452b12b3809397d1
Me <135455255+IcarussOne@users.noreply.github.com>
2024-08-02 05:34:41 -0500

    Finish Galacticraft compat

--- src/main/java/mod/emt/balkonsexpansion/compat/galacticraft/GalacticraftMaterialColors.java
+++ src/main/java/mod/emt/balkonsexpansion/compat/galacticraft/GalacticraftMaterialColors.java
@@ -3,26 +3,33 @@ package mod.emt.balkonsexpansion.compat.galacticraft;
 import ckathode.weaponmod.entity.projectile.ICustomProjectileMaterials;
 import ckathode.weaponmod.entity.projectile.MaterialRegistry;
 import ckathode.weaponmod.item.IItemWeapon;
+import micdoodle8.mods.galacticraft.core.GCItems;
+import micdoodle8.mods.galacticraft.planets.asteroids.items.AsteroidsItems;
 import micdoodle8.mods.galacticraft.planets.mars.items.MarsItems;
 import net.minecraft.item.ItemStack;
 
 // Some entities of BWM: Legacy such as flails can change their color, which is what this is used for.
 public class GalacticraftMaterialColors {
     public static final int MATERIAL_DESH = MarsItems.TOOLDESH.ordinal();
+    public static final int MATERIAL_HEAVY_DUTY = GCItems.TOOL_STEEL.ordinal();
+    public static final int MATERIAL_TITANIUM = AsteroidsItems.TOOL_TITANIUM.ordinal();
 
     public static void init() {
         MaterialRegistry.registerCustomProjectileMaterial(new ICustomProjectileMaterials() {
             @Override
             public int[] getAllMaterialIDs() {
-                return new int[]{MATERIAL_DESH};
+                return new int[]{MATERIAL_DESH, MATERIAL_HEAVY_DUTY, MATERIAL_TITANIUM};
             }
 
             @Override
             public int getMaterialID(ItemStack itemStack) {
                 if (itemStack != null && itemStack.getItem() instanceof IItemWeapon) {
                     IItemWeapon weapon = ((IItemWeapon) itemStack.getItem());
+                    
                     if (weapon.getMeleeComponent() != null) {
                         if (weapon.getMeleeComponent().weaponMaterial == MarsItems.TOOLDESH) return MATERIAL_DESH;
+                        if (weapon.getMeleeComponent().weaponMaterial == GCItems.TOOL_STEEL) return MATERIAL_HEAVY_DUTY;
+                        if (weapon.getMeleeComponent().weaponMaterial == AsteroidsItems.TOOL_TITANIUM) return MATERIAL_TITANIUM;
                     }
                 }
 
@@ -34,6 +41,9 @@ public class GalacticraftMaterialColors {
                 // The color for the given item tier must be returned as
                 // {R,G,B}, where each value is between 0 and 1.
                 if (i == MATERIAL_DESH) return new float[]{0.286F, 0.286F, 0.286F};
+                if (i == MATERIAL_HEAVY_DUTY) return new float[]{0.369F, 0.412F, 0.412F};
+                if (i == MATERIAL_TITANIUM) return new float[]{0.224F, 0.259F, 0.369F};
+                
                 return null;
             }
         });
--- src/main/java/mod/emt/balkonsexpansion/compat/galacticraft/GalacticraftRegistration.java
+++ src/main/java/mod/emt/balkonsexpansion/compat/galacticraft/GalacticraftRegistration.java
@@ -4,12 +4,12 @@ import ckathode.weaponmod.WMItemBuilder;
 import ckathode.weaponmod.item.ItemFlail;
 import ckathode.weaponmod.item.ItemMelee;
 import ckathode.weaponmod.item.ItemMusket;
+import micdoodle8.mods.galacticraft.core.GCItems;
+import micdoodle8.mods.galacticraft.planets.asteroids.items.AsteroidsItems;
 import micdoodle8.mods.galacticraft.planets.mars.items.MarsItems;
+import mod.emt.balkonsexpansion.BERegistry;
 import mod.emt.balkonsexpansion.BalkonsExpansion;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -17,17 +17,45 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 public class GalacticraftRegistration {
     public static ItemMelee battleaxeDesh;
+    public static ItemMelee battleaxeHeavyDuty;
+    public static ItemMelee battleaxeTitanium;
     public static ItemMelee boomerangDesh;
+    public static ItemMelee boomerangHeavyDuty;
+    public static ItemMelee boomerangTitanium;
     public static ItemMusket bayonetDesh;
+    public static ItemMusket bayonetHeavyDuty;
+    public static ItemMusket bayonetTitanium;
     public static ItemFlail flailDesh;
+    public static ItemFlail flailHeavyDuty;
+    public static ItemFlail flailTitanium;
     public static ItemMelee halberdDesh;
+    public static ItemMelee halberdHeavyDuty;
+    public static ItemMelee halberdTitanium;
     public static ItemMelee katanaDesh;
+    public static ItemMelee katanaHeavyDuty;
+    public static ItemMelee katanaTitanium;
     public static ItemMelee knifeDesh;
+    public static ItemMelee knifeHeavyDuty;
+    public static ItemMelee knifeTitanium;
     public static ItemMelee spearDesh;
+    public static ItemMelee spearHeavyDuty;
+    public static ItemMelee spearTitanium;
     public static ItemMelee warhammerDesh;
+    public static ItemMelee warhammerHeavyDuty;
+    public static ItemMelee warhammerTitanium;
 
     public static void registerItems(RegistryEvent.Register<Item> event) {
         IForgeRegistry<Item> registry = event.getRegistry();
+
+        registry.register(spearHeavyDuty = WMItemBuilder.createStandardSpear(BalkonsExpansion.MOD_ID, "spear.heavy_duty", GCItems.TOOL_STEEL));
+        registry.register(halberdHeavyDuty = WMItemBuilder.createStandardHalberd(BalkonsExpansion.MOD_ID, "halberd.heavy_duty", GCItems.TOOL_STEEL));
+        registry.register(battleaxeHeavyDuty = WMItemBuilder.createStandardBattleaxe(BalkonsExpansion.MOD_ID, "battleaxe.heavy_duty", GCItems.TOOL_STEEL));
+        registry.register(knifeHeavyDuty = WMItemBuilder.createStandardKnife(BalkonsExpansion.MOD_ID, "knife.heavy_duty", GCItems.TOOL_STEEL));
+        registry.register(warhammerHeavyDuty = WMItemBuilder.createStandardWarhammer(BalkonsExpansion.MOD_ID, "warhammer.heavy_duty", GCItems.TOOL_STEEL));
+        registry.register(flailHeavyDuty = WMItemBuilder.createStandardFlail(BalkonsExpansion.MOD_ID, "flail.heavy_duty", GCItems.TOOL_STEEL));
+        registry.register(katanaHeavyDuty = WMItemBuilder.createStandardKatana(BalkonsExpansion.MOD_ID, "katana.heavy_duty", GCItems.TOOL_STEEL));
+        registry.register(boomerangHeavyDuty = WMItemBuilder.createStandardBoomerang(BalkonsExpansion.MOD_ID, "boomerang.heavy_duty", GCItems.TOOL_STEEL));
+        registry.register(bayonetHeavyDuty = WMItemBuilder.createStandardMusketWithBayonet(BalkonsExpansion.MOD_ID, "musketbayonet.heavy_duty", GCItems.TOOL_STEEL, knifeHeavyDuty));
 
         registry.register(spearDesh = WMItemBuilder.createStandardSpear(BalkonsExpansion.MOD_ID, "spear.desh", MarsItems.TOOLDESH));
         registry.register(halberdDesh = WMItemBuilder.createStandardHalberd(BalkonsExpansion.MOD_ID, "halberd.desh", MarsItems.TOOLDESH));
@@ -38,18 +66,46 @@ public class GalacticraftRegistration {
         registry.register(katanaDesh = WMItemBuilder.createStandardKatana(BalkonsExpansion.MOD_ID, "katana.desh", MarsItems.TOOLDESH));
         registry.register(boomerangDesh = WMItemBuilder.createStandardBoomerang(BalkonsExpansion.MOD_ID, "boomerang.desh", MarsItems.TOOLDESH));
         registry.register(bayonetDesh = WMItemBuilder.createStandardMusketWithBayonet(BalkonsExpansion.MOD_ID, "musketbayonet.desh", MarsItems.TOOLDESH, knifeDesh));
+
+        registry.register(spearTitanium = WMItemBuilder.createStandardSpear(BalkonsExpansion.MOD_ID, "spear.titanium", AsteroidsItems.TOOL_TITANIUM));
+        registry.register(halberdTitanium = WMItemBuilder.createStandardHalberd(BalkonsExpansion.MOD_ID, "halberd.titanium", AsteroidsItems.TOOL_TITANIUM));
+        registry.register(battleaxeTitanium = WMItemBuilder.createStandardBattleaxe(BalkonsExpansion.MOD_ID, "battleaxe.titanium", AsteroidsItems.TOOL_TITANIUM));
+        registry.register(knifeTitanium = WMItemBuilder.createStandardKnife(BalkonsExpansion.MOD_ID, "knife.titanium", AsteroidsItems.TOOL_TITANIUM));
+        registry.register(warhammerTitanium = WMItemBuilder.createStandardWarhammer(BalkonsExpansion.MOD_ID, "warhammer.titanium", AsteroidsItems.TOOL_TITANIUM));
+        registry.register(flailTitanium = WMItemBuilder.createStandardFlail(BalkonsExpansion.MOD_ID, "flail.titanium", AsteroidsItems.TOOL_TITANIUM));
+        registry.register(katanaTitanium = WMItemBuilder.createStandardKatana(BalkonsExpansion.MOD_ID, "katana.titanium", AsteroidsItems.TOOL_TITANIUM));
+        registry.register(boomerangTitanium = WMItemBuilder.createStandardBoomerang(BalkonsExpansion.MOD_ID, "boomerang.titanium", AsteroidsItems.TOOL_TITANIUM));
+        registry.register(bayonetTitanium = WMItemBuilder.createStandardMusketWithBayonet(BalkonsExpansion.MOD_ID, "musketbayonet.titanium", AsteroidsItems.TOOL_TITANIUM, knifeTitanium));
     }
 
     @SideOnly(Side.CLIENT)
     public static void registerRenderersItem() {
-        ModelLoader.setCustomModelResourceLocation(battleaxeDesh, 0, new ModelResourceLocation(new ResourceLocation(BalkonsExpansion.MOD_ID, battleaxeDesh.getTranslationKey().substring(5)), "inventory"));
-        ModelLoader.setCustomModelResourceLocation(boomerangDesh, 0, new ModelResourceLocation(new ResourceLocation(BalkonsExpansion.MOD_ID, boomerangDesh.getTranslationKey().substring(5)), "inventory"));
-        ModelLoader.setCustomModelResourceLocation(bayonetDesh, 0, new ModelResourceLocation(new ResourceLocation(BalkonsExpansion.MOD_ID, bayonetDesh.getTranslationKey().substring(5)), "inventory"));
-        ModelLoader.setCustomModelResourceLocation(flailDesh, 0, new ModelResourceLocation(new ResourceLocation(BalkonsExpansion.MOD_ID, flailDesh.getTranslationKey().substring(5)), "inventory"));
-        ModelLoader.setCustomModelResourceLocation(halberdDesh, 0, new ModelResourceLocation(new ResourceLocation(BalkonsExpansion.MOD_ID, halberdDesh.getTranslationKey().substring(5)), "inventory"));
-        ModelLoader.setCustomModelResourceLocation(katanaDesh, 0, new ModelResourceLocation(new ResourceLocation(BalkonsExpansion.MOD_ID, katanaDesh.getTranslationKey().substring(5)), "inventory"));
-        ModelLoader.setCustomModelResourceLocation(knifeDesh, 0, new ModelResourceLocation(new ResourceLocation(BalkonsExpansion.MOD_ID, knifeDesh.getTranslationKey().substring(5)), "inventory"));
-        ModelLoader.setCustomModelResourceLocation(spearDesh, 0, new ModelResourceLocation(new ResourceLocation(BalkonsExpansion.MOD_ID, spearDesh.getTranslationKey().substring(5)), "inventory"));
-        ModelLoader.setCustomModelResourceLocation(warhammerDesh, 0, new ModelResourceLocation(new ResourceLocation(BalkonsExpansion.MOD_ID, warhammerDesh.getTranslationKey().substring(5)), "inventory"));
+        BERegistry.registerWeaponItemModel(battleaxeDesh);
+        BERegistry.registerWeaponItemModel(battleaxeHeavyDuty);
+        BERegistry.registerWeaponItemModel(battleaxeTitanium);
+        BERegistry.registerWeaponItemModel(boomerangDesh);
+        BERegistry.registerWeaponItemModel(boomerangHeavyDuty);
+        BERegistry.registerWeaponItemModel(boomerangTitanium);
+        BERegistry.registerWeaponItemModel(bayonetDesh);
+        BERegistry.registerWeaponItemModel(bayonetHeavyDuty);
+        BERegistry.registerWeaponItemModel(bayonetTitanium);
+        BERegistry.registerWeaponItemModel(flailDesh);
+        BERegistry.registerWeaponItemModel(flailHeavyDuty);
+        BERegistry.registerWeaponItemModel(flailTitanium);
+        BERegistry.registerWeaponItemModel(halberdDesh);
+        BERegistry.registerWeaponItemModel(halberdHeavyDuty);
+        BERegistry.registerWeaponItemModel(halberdTitanium);
+        BERegistry.registerWeaponItemModel(katanaDesh);
+        BERegistry.registerWeaponItemModel(katanaHeavyDuty);
+        BERegistry.registerWeaponItemModel(katanaTitanium);
+        BERegistry.registerWeaponItemModel(knifeDesh);
+        BERegistry.registerWeaponItemModel(knifeHeavyDuty);
+        BERegistry.registerWeaponItemModel(knifeTitanium);
+        BERegistry.registerWeaponItemModel(spearDesh);
+        BERegistry.registerWeaponItemModel(spearHeavyDuty);
+        BERegistry.registerWeaponItemModel(spearTitanium);
+        BERegistry.registerWeaponItemModel(warhammerDesh);
+        BERegistry.registerWeaponItemModel(warhammerHeavyDuty);
+        BERegistry.registerWeaponItemModel(warhammerTitanium);
     }
 }
